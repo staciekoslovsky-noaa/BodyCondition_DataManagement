6a94f8c75f577a93611cdf17e333917b3b78fa52
staciekozhardy <stacie.hardy@noaa.gov>
2022-05-02 17:30:25 -0700

    Update code to iterate years
    Updated code to iterate through years of data and new folder structure

--- BodyCondition_00_ImportImageExif.R
+++ BodyCondition_00_ImportImageExif.R
@@ -2,7 +2,8 @@
 # S. Hardy
 
 # Set Working Variables
-wd <- "\\\\akc0ss-n086\\NMML_Polar\\Data\\UAS\\UAS_BodyCondition\\Data\\"
+wd <- "\\\\akc0ss-n086\\NMML_Polar\\Data\\UAS\\UAS_BodyCondition\\Data"
+years <- c(2021, 2022)
 
 # Create functions -----------------------------------------------
 # Function to install packages needed
@@ -38,81 +39,83 @@ con <- RPostgreSQL::dbConnect(PostgreSQL(),
                               user = Sys.getenv("pep_admin"), 
                               rstudioapi::askForPassword(paste("Enter your DB password for user account: ", Sys.getenv("pep_admin"), sep = "")))
 
-# RPostgreSQL::dbSendQuery(con, "DELETE FROM body_condition.geo_images")
+RPostgreSQL::dbSendQuery(con, "DELETE FROM body_condition.geo_images")
 
 imported <- RPostgreSQL::dbGetQuery(con, "SELECT DISTINCT image_name, \'y\' as in_database FROM body_condition.geo_images")
 
-# Create list of camera folders within which data need to be processed 
-dir <- list.dirs(wd, full.names = TRUE, recursive = FALSE)
-dir <- data.frame(path = dir[grep("[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]$", dir)], stringsAsFactors = FALSE) 
-dir <- dir %>%
-  mutate(path = paste(path, 'Images', sep = "/"))
-
-for (i in 1:nrow(dir)){
-  # Process if folder exists
-  if(!dir.exists(dir$path[i])) next 
+for (y in 1:length(years)) {
+  # Create list of camera folders within which data need to be processed 
+  dir <- list.dirs(paste(wd, years[y], sep = "/"), full.names = TRUE, recursive = FALSE)
+  dir <- data.frame(path = dir[grep("[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]$", dir)], stringsAsFactors = FALSE) 
+  dir <- dir %>%
+    mutate(path = paste(path, 'Images', sep = "/"))
   
-  # Get next ID for images data
-  processed_id <- RPostgreSQL::dbGetQuery(con, "SELECT max(id) FROM body_condition.geo_images")
-  processed_id$max <- ifelse(is.na(processed_id$max), 0, processed_id$max)
-  
-  # Get list of images, join to imported data, and identify images that have not been imported to the DB
-  images <- list.files(dir$path[i], pattern = "jpg$|JPG$|dng$|DNG$", full.names = TRUE, recursive = FALSE)
-  images <- data.frame(path = images, stringsAsFactors = FALSE)
-  images <- images %>%
-    mutate(image_name = basename(path)) %>%
-    left_join(imported, by = "image_name") %>%
-    filter(is.na(in_database))
+  for (i in 1:nrow(dir)){
+    # Process if folder exists
+    if(!dir.exists(dir$path[i])) next 
     
-  # Process and import data if any images remain in list
-  if (nrow(images) > 0) {
-    original_exif <- exifr::read_exif("\\\\akc0ss-n086\\NMML_Polar\\Data\\UAS\\UAS_BodyCondition\\Data\\test_exif_DO_NOT_DELETE.JPG", tags = tags)
-    original_exif <- data.frame(SourceFile = original_exif[0, c(1:2)], stringsAsFactors = FALSE)
+    # Get next ID for images data
+    processed_id <- RPostgreSQL::dbGetQuery(con, "SELECT max(id) FROM body_condition.geo_images")
+    processed_id$max <- ifelse(is.na(processed_id$max), 0, processed_id$max)
     
-    temp_exif <- exifr::read_exif(images$path, tags = tags)
-    temp_exif <- data.frame(lapply(temp_exif, as.character), stringsAsFactors = FALSE)
+    # Get list of images, join to imported data, and identify images that have not been imported to the DB
+    images <- list.files(dir$path[i], pattern = "jpg$|JPG$|dng$|DNG$", full.names = TRUE, recursive = FALSE)
+    images <- data.frame(path = images, stringsAsFactors = FALSE)
+    images <- images %>%
+      mutate(image_name = basename(path)) %>%
+      left_join(imported, by = "image_name") %>%
+      filter(is.na(in_database))
     
-    # Check for missing columns
-    missing <- setdiff(tags, names(temp_exif)) 
-    temp_exif[missing] <- ''                   
-    temp_exif <- temp_exif[tags]
-    
-    # Merge with other exif data
-    original_exif <- bind_rows(original_exif, temp_exif)
-    rm(temp_exif)
-    
-    original_exif <- original_exif %>%
-      mutate(id = 1:n() + processed_id$max,
-             image_name = FileName,
-             image_path = SourceFile,
-             exif_image_dt = format(lubridate::ymd_hms(DateTimeOriginal, tz = "America/Vancouver"), tz = "UTC"),
-             exif_latitude = GPSLatitude,
-             exif_longitude = GPSLongitude,
-             exif_altitude_m = as.numeric(GPSAltitude),
-             exif_heading = as.numeric(FlightYawDegree),
-             exif_pitch = as.numeric(FlightPitchDegree),
-             exif_roll = as.numeric(FlightRollDegree),
-             exif_gimbal_heading = as.numeric(GimbalYawDegree),
-             exif_gimbal_pitch = as.numeric(GimbalPitchDegree),
-             exif_gimbal_roll = as.numeric(GimbalRollDegree),
-             exif_lens = LensInfo,
-             exif_zoom_factor = as.numeric(DigitalZoomRatio),
-             for_calibration_lku = 'Q',
-             measurement_status_lku = 'Q',
-             use_for_model_lku = 'Q',
-             exif_relative_altitude_m = RelativeAltitude,
-             file_access_dt = format(lubridate::ymd_hms(FileAccessDate, tz = "America/Vancouver"), tz = "UTC"),
-             geom = "0101000020E610000000000000000000000000000000000000") %>%
-      select(id, image_name, image_path,
-             exif_image_dt, exif_latitude, exif_longitude, exif_altitude_m,
-             exif_heading, exif_pitch, exif_roll,
-             exif_gimbal_heading, exif_gimbal_pitch, exif_gimbal_roll,
-             exif_lens, exif_zoom_factor,
-             for_calibration_lku, measurement_status_lku, use_for_model_lku,
-             exif_relative_altitude_m, file_access_dt, geom)
-    
-    # Write data to the DB
-    RPostgreSQL::dbWriteTable(con, c("body_condition", "geo_images"), original_exif, append = TRUE, row.names = FALSE)
+    # Process and import data if any images remain in list
+    if (nrow(images) > 0) {
+      original_exif <- exifr::read_exif("\\\\akc0ss-n086\\NMML_Polar\\Data\\UAS\\UAS_BodyCondition\\Data\\test_exif_DO_NOT_DELETE.JPG", tags = tags)
+      original_exif <- data.frame(SourceFile = original_exif[0, c(1:2)], stringsAsFactors = FALSE)
+      
+      temp_exif <- exifr::read_exif(images$path, tags = tags)
+      temp_exif <- data.frame(lapply(temp_exif, as.character), stringsAsFactors = FALSE)
+      
+      # Check for missing columns
+      missing <- setdiff(tags, names(temp_exif)) 
+      temp_exif[missing] <- ''                   
+      temp_exif <- temp_exif[tags]
+      
+      # Merge with other exif data
+      original_exif <- bind_rows(original_exif, temp_exif)
+      rm(temp_exif)
+      
+      original_exif <- original_exif %>%
+        mutate(id = 1:n() + processed_id$max,
+               image_name = FileName,
+               image_path = SourceFile,
+               exif_image_dt = format(lubridate::ymd_hms(DateTimeOriginal, tz = "America/Vancouver"), tz = "UTC"),
+               exif_latitude = GPSLatitude,
+               exif_longitude = GPSLongitude,
+               exif_altitude_m = as.numeric(GPSAltitude),
+               exif_heading = as.numeric(FlightYawDegree),
+               exif_pitch = as.numeric(FlightPitchDegree),
+               exif_roll = as.numeric(FlightRollDegree),
+               exif_gimbal_heading = as.numeric(GimbalYawDegree),
+               exif_gimbal_pitch = as.numeric(GimbalPitchDegree),
+               exif_gimbal_roll = as.numeric(GimbalRollDegree),
+               exif_lens = LensInfo,
+               exif_zoom_factor = as.numeric(DigitalZoomRatio),
+               for_calibration_lku = 'Q',
+               measurement_status_lku = 'Q',
+               use_for_model_lku = 'Q',
+               exif_relative_altitude_m = RelativeAltitude,
+               file_access_dt = format(lubridate::ymd_hms(FileAccessDate, tz = "America/Vancouver"), tz = "UTC"),
+               geom = "0101000020E610000000000000000000000000000000000000") %>%
+        select(id, image_name, image_path,
+               exif_image_dt, exif_latitude, exif_longitude, exif_altitude_m,
+               exif_heading, exif_pitch, exif_roll,
+               exif_gimbal_heading, exif_gimbal_pitch, exif_gimbal_roll,
+               exif_lens, exif_zoom_factor,
+               for_calibration_lku, measurement_status_lku, use_for_model_lku,
+               exif_relative_altitude_m, file_access_dt, geom)
+      
+      # Write data to the DB
+      RPostgreSQL::dbWriteTable(con, c("body_condition", "geo_images"), original_exif, append = TRUE, row.names = FALSE)
+    }
   }
 }
 
--- BodyCondition_00_ImportLaserRangefinderData.R
+++ BodyCondition_00_ImportLaserRangefinderData.R
@@ -2,7 +2,8 @@
 # S. Hardy
 
 # Set Working Variables
-wd <- "O:\\Data\\UAS\\UAS_BodyCondition"
+wd <- "O:\\Data\\UAS\\UAS_BodyCondition\\Data"
+years <- c(2021, 2022)
 
 # Create functions -----------------------------------------------
 # Function to install packages needed
@@ -18,6 +19,7 @@ install_pkg <- function(x)
 # Install libraries ----------------------------------------------
 install_pkg("RPostgreSQL")
 install_pkg("tidyverse")
+install_pkg("lubridate")
 
 # Run code -------------------------------------------------------
 setwd(wd)
@@ -36,74 +38,115 @@ RPostgreSQL::dbSendQuery(con, "DELETE FROM body_condition.geo_lrf_feed")
 
 imported <- RPostgreSQL::dbGetQuery(con, "SELECT DISTINCT lrf_file_name FROM body_condition.geo_lrf_feed")
 
-# Create list of camera folders within which data need to be processed 
-dir <- list.dirs(wd, full.names = TRUE, recursive = FALSE)
-dir <- data.frame(path = dir[grep("[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]$", dir)], stringsAsFactors = FALSE) 
-dir <- dir %>%
-  mutate(path = paste(path, 'LRF_data', sep = "/"))
-
-for (i in 1:nrow(dir)){
-  if(!dir.exists(dir$path[i])) next 
-  files <- list.files(dir$path[i], full.names = TRUE, recursive = FALSE)
-  files <- data.frame(path = files, stringsAsFactors = FALSE)
-  files <- files %>%
-    mutate(file_name = basename(path))
+for (y in 1:length(years)) {
+  # Create list of camera folders within which data need to be processed 
+  dir <- list.dirs(paste(wd, years[y], sep = "/"), full.names = TRUE, recursive = FALSE)
+  dir <- data.frame(path = dir[grep("[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]$", dir)], stringsAsFactors = FALSE) 
+  dir <- dir %>%
+    mutate(path = paste(path, 'LRF_data', sep = "/"))
   
-  for (j in 1:nrow(files)){
-    # Check if file already imported
-    if(files$file_name[j] %in% imported$lrf_file_name) next
+  for (i in 1:nrow(dir)){
+    if(!dir.exists(dir$path[i])) next 
+    files <- list.files(dir$path[i], full.names = TRUE, recursive = FALSE)
+    files <- data.frame(path = files, stringsAsFactors = FALSE)
+    files <- files %>%
+      mutate(file_name = basename(path))
     
-    processed_id <- RPostgreSQL::dbGetQuery(con, "SELECT max(id) FROM body_condition.geo_lrf_feed")
-    processed_id$max <- ifelse(is.na(processed_id$max), 0, processed_id$max)
-    
-    # Process and import data
-    lrf <- read.table(files$path[j], sep = ",", col.names = c("utc_gps_time", "utc_gps_date", "laser_range_raw_m", "laser_range_median", "laser_range_first_median", "laser_range_last_median", 
-                                                              "laser_signal_strength_first", "laser_signal_strength_last", "laser_background_noise", "laser_lost_signal_confirmation", 
-                                                              "gps_lat", "gps_lat_ns", "gps_lon", "gps_lon_ew", "gps_speed", "gps_track_angle", "gps_magvar", "test01", "test02", "test03"), 
-                      skip = 1, header = FALSE, stringsAsFactors = FALSE, blank.lines.skip = TRUE, colClasses = "character", dec = ".", fill = TRUE, strip.white = TRUE)
-    
-    lrf <- lrf %>%
-      filter(laser_range_raw_m != "-1.00") %>% 
-      filter(utc_gps_date != '//') %>%
-      filter(gps_lat != "") %>%
-      filter(nchar(gps_lat_ns) == 1) %>%
-      filter(gps_lon != "") %>%
-      filter(nchar(gps_lon_ew) == 1) %>%
-      filter(gps_magvar != "$GPRMC") %>%
-      mutate(id = 1:n() + processed_id$max,
-             lrf_file_name = files$file_name[j],
-             gps_dt = ymd_hms(as.POSIXct(paste(utc_gps_date, utc_gps_time, sep = " "), format = "%m/%d/%y %H:%M:%OS", tz = "UTC"), tz = "UTC"),
-             laser_range_raw_m = as.numeric(laser_range_raw_m),
-             laser_range_median = as.numeric(laser_range_median),
-             laser_range_first_median = as.numeric(laser_range_first_median),
-             laser_range_last_median = as.numeric(laser_range_last_median),
-             laser_signal_strength_first = as.numeric(laser_signal_strength_first),
-             laser_signal_strength_last = as.numeric(laser_signal_strength_last),
-             laser_background_noise = as.numeric(laser_background_noise),
-             laser_lost_signal_confirmation = as.numeric(laser_lost_signal_confirmation),
-             gps_latitude = round(as.numeric(stringr::str_sub(gps_lat, 1, 2)) + as.numeric(stringr::str_sub(gps_lat, -nchar(gps_lat) + 2, -1))/60, 9),
-             gps_longitude = round(as.numeric(ifelse(gps_lon_ew == "W", 
-               -(as.numeric(stringr::str_sub(gps_lon, 1, 3)) + as.numeric(stringr::str_sub(gps_lon, -nchar(gps_lon) + 3, -1))/60),
-               as.numeric(stringr::str_sub(gps_lon, 1, 3)) + as.numeric(stringr::str_sub(gps_lon, -nchar(gps_lon) + 3, -1))/60)), 9),
-             qa_status_lku = 'U',
-             geom = "0101000020E610000000000000000000000000000000000000") %>%
-      # Set numeric NA values to -99
-      mutate(laser_range_raw_m = ifelse(is.na(laser_range_raw_m), -99, laser_range_raw_m),
-             laser_range_median = ifelse(is.na(laser_range_median), -99, laser_range_median),
-             laser_range_first_median = ifelse(is.na(laser_range_first_median), -99, laser_range_first_median),
-             laser_range_last_median = ifelse(is.na(laser_range_last_median), -99, laser_range_last_median),
-             laser_signal_strength_first = ifelse(is.na(laser_signal_strength_first), -99, laser_signal_strength_first),
-             laser_signal_strength_last = ifelse(is.na(laser_signal_strength_last), -99, laser_signal_strength_last),
-             laser_background_noise = ifelse(is.na(laser_background_noise), -99, laser_background_noise),
-             laser_lost_signal_confirmation = ifelse(is.na(laser_lost_signal_confirmation), -99, laser_lost_signal_confirmation),
-      ) %>%
-      select(id, lrf_file_name, gps_dt, laser_range_raw_m, laser_range_median, laser_range_first_median, laser_range_last_median,
-            laser_signal_strength_first, laser_signal_strength_last, laser_background_noise, laser_lost_signal_confirmation,
-            gps_latitude, gps_longitude, qa_status_lku, geom
-            )
-    
-    # Write data to the DB
-    RPostgreSQL::dbWriteTable(con, c("body_condition", "geo_lrf_feed"), lrf, append = TRUE, row.names = FALSE)
+    for (j in 1:nrow(files)){
+      # Check if file already imported
+      if(files$file_name[j] %in% imported$lrf_file_name) next
+      
+      processed_id <- RPostgreSQL::dbGetQuery(con, "SELECT max(id) FROM body_condition.geo_lrf_feed")
+      processed_id$max <- ifelse(is.na(processed_id$max), 0, processed_id$max)
+      
+      # Process and import data
+      
+      
+      if (years[y] == 2021) {
+        lrf <- read.table(files$path[j], sep = ",", col.names = c("utc_gps_time", "utc_gps_date", "laser_range_raw_m", "laser_range_median", "laser_range_first_median", "laser_range_last_median", 
+                                                                  "laser_signal_strength_first", "laser_signal_strength_last", "laser_background_noise", "laser_lost_signal_confirmation", 
+                                                                  "gps_lat", "gps_lat_ns", "gps_lon", "gps_lon_ew", "gps_speed", "gps_track_angle", "gps_magvar", "test01", "test02", "test03"), 
+                          skip = 1, header = FALSE, stringsAsFactors = FALSE, blank.lines.skip = TRUE, colClasses = "character", dec = ".", fill = TRUE, strip.white = TRUE)
+        lrf <- lrf %>%
+          filter(laser_range_raw_m != "-1.00") %>% 
+          filter(utc_gps_date != '//') %>%
+          filter(gps_lat != "") %>%
+          filter(nchar(gps_lat_ns) == 1) %>%
+          filter(gps_lon != "") %>%
+          filter(nchar(gps_lon_ew) == 1) %>%
+          filter(gps_magvar != "$GPRMC") %>%
+          mutate(id = 1:n() + processed_id$max,
+                 lrf_file_name = files$file_name[j],
+                 gps_dt = ymd_hms(as.POSIXct(paste(utc_gps_date, utc_gps_time, sep = " "), format = "%m/%d/%y %H:%M:%OS", tz = "UTC"), tz = "UTC"),
+                 laser_range_raw_m = as.numeric(laser_range_raw_m),
+                 laser_range_median = as.numeric(laser_range_median),
+                 gps_latitude = round(as.numeric(stringr::str_sub(gps_lat, 1, 2)) + as.numeric(stringr::str_sub(gps_lat, -nchar(gps_lat) + 2, -1))/60, 9),
+                 gps_longitude = round(as.numeric(ifelse(gps_lon_ew == "W", 
+                                                         -(as.numeric(stringr::str_sub(gps_lon, 1, 3)) + as.numeric(stringr::str_sub(gps_lon, -nchar(gps_lon) + 3, -1))/60),
+                                                         as.numeric(stringr::str_sub(gps_lon, 1, 3)) + as.numeric(stringr::str_sub(gps_lon, -nchar(gps_lon) + 3, -1))/60)), 9),
+                 qa_status_lku = 'U',
+                 geom = "0101000020E610000000000000000000000000000000000000") %>%
+          # Set numeric NA values to -99
+          mutate(laser_range_raw_m = ifelse(is.na(laser_range_raw_m), -99, laser_range_raw_m),
+                 laser_range_median = ifelse(is.na(laser_range_median), -99, laser_range_median),
+                 gps_speed = ifelse(is.na(gps_speed), -99, gps_speed)) %>%
+          select(id, lrf_file_name, gps_dt, laser_range_raw_m, laser_range_median, gps_latitude, gps_longitude, gps_speed, qa_status_lku, geom
+          )
+      } else {
+        lrf <- read.table(files$path[j], sep = ",", col.names = c("utc_gps_time", "utc_gps_date", "laser_range_raw_m", "laser_range_median", 
+                                                                  "gps_lat", "gps_lat_ns", "gps_lon", "gps_lon_ew", "gps_speed", 
+                                                                  "imu_pitch", "imu_roll",
+                                                                  "raw_gyro_x",	"raw_gyro_y",	"raw_gyro_z",	"raw_accel_x",	"raw_accel_y",	"raw_accel_z",
+                                                                  "test01", "test02", "test03"), 
+                          skip = 1, header = FALSE, stringsAsFactors = FALSE, blank.lines.skip = TRUE, colClasses = "character", dec = ".", fill = TRUE, strip.white = TRUE)
+        lrf <- lrf %>%
+          filter(laser_range_raw_m != "-1.00") %>% 
+          filter(utc_gps_date != '//') %>%
+          filter(gps_lat != "") %>%
+          filter(nchar(gps_lat_ns) == 1) %>%
+          filter(gps_lon != "") %>%
+          filter(nchar(gps_lon_ew) == 1) %>%
+          filter(gps_magvar != "$GPRMC") %>%
+          mutate(id = 1:n() + processed_id$max,
+                 lrf_file_name = files$file_name[j],
+                 gps_dt = ymd_hms(as.POSIXct(paste(utc_gps_date, utc_gps_time, sep = " "), format = "%m/%d/%y %H:%M:%OS", tz = "UTC"), tz = "UTC"),
+                 laser_range_raw_m = as.numeric(laser_range_raw_m),
+                 laser_range_median = as.numeric(laser_range_median),
+                 gps_latitude = round(as.numeric(stringr::str_sub(gps_lat, 1, 2)) + as.numeric(stringr::str_sub(gps_lat, -nchar(gps_lat) + 2, -1))/60, 9),
+                 gps_longitude = round(as.numeric(ifelse(gps_lon_ew == "W", 
+                                                         -(as.numeric(stringr::str_sub(gps_lon, 1, 3)) + as.numeric(stringr::str_sub(gps_lon, -nchar(gps_lon) + 3, -1))/60),
+                                                         as.numeric(stringr::str_sub(gps_lon, 1, 3)) + as.numeric(stringr::str_sub(gps_lon, -nchar(gps_lon) + 3, -1))/60)), 9),
+                 gps_speed = as.numeric(gps_speed),
+                 imu_pitch = as.numeric(imu_pitch),
+                 imu_roll = as.numeric(imu_roll),
+                 raw_gyro_x = as.numeric(raw_gyro_x),
+                 raw_gyro_y = as.numeric(raw_gyro_y),
+                 raw_gyro_z = as.numeric(raw_gyro_z),
+                 raw_accel_x = as.numeric(raw_accel_x),
+                 raw_accel_y = as.numeric(raw_accel_y),
+                 raw_accel_z = as.numeric(raw_accel_z),
+                 qa_status_lku = 'U',
+                 geom = "0101000020E610000000000000000000000000000000000000") %>%
+          # Set numeric NA values to -99
+          mutate(laser_range_raw_m = ifelse(is.na(laser_range_raw_m), -99, laser_range_raw_m),
+                 laser_range_median = ifelse(is.na(laser_range_median), -99, laser_range_median),
+                 gps_speed = ifelse(is.na(gps_speed), -99, gps_speed),
+                 imu_pitch = ifelse(is.na(imu_pitch), -99, imu_pitch),
+                 imu_roll = ifelse(is.na(imu_roll), -99, imu_roll),
+                 raw_gyro_x = ifelse(is.na(raw_gyro_x), -99, raw_gyro_x),
+                 raw_gyro_y = ifelse(is.na(raw_gyro_y), -99, raw_gyro_y),
+                 raw_gyro_z = ifelse(is.na(raw_gyro_z), -99, raw_gyro_z),
+                 raw_accel_x = ifelse(is.na(raw_accel_x), -99, raw_accel_x),
+                 raw_accel_y = ifelse(is.na(raw_accel_y), -99, raw_accel_y),
+                 raw_accel_z = ifelse(is.na(raw_accel_z), -99, raw_accel_z),) %>%
+          select(id, lrf_file_name, gps_dt, laser_range_raw_m, laser_range_median, gps_latitude, gps_longitude, gps_speed, imu_pitch, imu_roll, 
+                 raw_gyro_x, raw_gyro_y, raw_gyro_z, raw_accel_x, raw_accel_y, raw_accel_z,
+                 qa_status_lku, geom
+          )
+      }
+      # Write data to the DB
+      RPostgreSQL::dbWriteTable(con, c("body_condition", "geo_lrf_feed"), lrf, append = TRUE, row.names = FALSE)
+    }
   }
 }
 
